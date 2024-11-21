5dacbf51116648661963739f08be038d723404dd
glygoric <sergej.gligoric@gmail.com>
2024-11-21 13:23:18 +0100

    UPDATE ON REGISTER

--- hamburger.js
+++ hamburger.js
@@ -7,6 +7,7 @@ document.getElementById("menu-deactivator").onclick=()=>{
     document.getElementById("menu").style.width="0px";
 }
 window.onscroll=()=>{
+    document.getElementById("nav-id").style.backgroundColor = window.scrollY > 0 ? "rgba(102,16,242,0.4)" : "#6610f2";
     document.getElementById("menu").style.left="-100px";
     document.getElementById("menu").style.width="0px";
 }
